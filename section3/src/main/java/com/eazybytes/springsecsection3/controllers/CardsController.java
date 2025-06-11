e544bfe54b466db4e169c111d4be9e051957bfac
Tarachand90 <tarachand90@gmail.com>
2025-06-11 12:46:37 +0530

    migrate codebase from section2 to section3 and move user credentials from config to in-memory UserDetailsService bean
    - Renamed package structure from springsecsection2 to springsecsection3
    - Updated application main class, configs, and all controllers accordingly
    - Renamed test class to match new section
    - Modified application.yml to reflect user config changes
    - Updated pom.xml for necessary adjustments
    - Removed SPRING_SECURITY_USER_NAME and SPRING_SECURITY_USER_PASSWORD from application.yml
    - Introduced an in-memory UserDetailsService bean with static users 'user' and 'admin'
    - Preserved application name and logging configurations

--- section3/src/main/java/com/eazybytes/springsecsection3/controllers/CardsController.java
+++ section3/src/main/java/com/eazybytes/springsecsection3/controllers/CardsController.java
@@ -1,4 +1,4 @@
-package com.eazybytes.springsecsection1.controllers;
+package com.eazybytes.springsecsection3.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
